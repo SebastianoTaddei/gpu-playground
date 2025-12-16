a170ef01d5eaa1cf163045c9f2546f652c09734e
Sebastiano Taddei <61469576+SebastianoTaddei@users.noreply.github.com>
2025-12-16 16:17:38 +0100

    feat: first draft for tests

---
+++ README.md
@@ -0,0 +1,3 @@
+# Metal Tests
+
+Various tests for GPU compute using Metal, with a focus on linear algebra.
---
+++ cmake/eigen3.cmake
@@ -0,0 +1,33 @@
+set(EIGEN_REQUIRED_VERSION 3.4.0)
+cmake_policy(SET CMP0135 NEW)
+
+list(APPEND CMAKE_PREFIX_PATH "${METAL_TESTS_THIRD_PARTY_DIR}")
+find_package(
+  Eigen3
+  ${EIGEN_REQUIRED_VERSION}
+  NO_MODULE
+  QUIET
+)
+
+if(NOT TARGET Eigen3::Eigen)
+  message(STATUS
+    "Metal Tests: "
+    "Did not find Eigen ${EIGEN_REQUIRED_VERSION} installed, "
+    "downloading to ${BSPLINEX_THIRD_PARTY_DIR}"
+  )
+  include(FetchContent)
+
+  set(FETCHCONTENT_BASE_DIR "${METAL_TESTS_THIRD_PARTY_DIR}")
+  fetchcontent_declare(
+      Eigen3
+      URL "https://gitlab.com/libeigen/eigen/-/archive/${EIGEN_REQUIRED_VERSION}/eigen-${EIGEN_REQUIRED_VERSION}.tar.gz"
+  )
+
+  fetchcontent_makeavailable(Eigen3)
+else()
+  get_target_property(EIGEN_INCLUDE_DIRS
+    Eigen3::Eigen
+    INTERFACE_INCLUDE_DIRECTORIES
+  )
+  message(STATUS "Metal Tests: Found Eigen installed in ${EIGEN_INCLUDE_DIRS}")
+endif()

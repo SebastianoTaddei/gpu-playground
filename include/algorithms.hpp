2dd3a9568b2cbac7b6a65a53e24ddfc45a9377db
Sebastiano Taddei <61469576+SebastianoTaddei@users.noreply.github.com>
2026-01-15 22:05:38 +0100

    feat: added conjugate gradient
    And better written exit conditions and temporaries

--- include/algorithms.hpp
+++ include/algorithms.hpp
@@ -21,17 +21,51 @@ inline Tensor gradient_descent(
 
   for (size_t i{0}; i < max_iter; i++)
   {
-    auto const r_t  = r.transpose();
-    auto const r_e  = r_t * r;
-    auto const eta  = r_e.cdiv(r_t * a * r);
-    x_res          += r.smul(eta);
-
+    auto const r_t = r.transpose();
+    auto const r_e = r_t * r;
     if (std::sqrt(r_e.cpu().front()) < tol)
     {
       return x_res;
     }
 
-    r -= (a * r).smul(eta);
+    auto const ar   = a * r;
+    auto const eta  = r_e.cdiv(r_t * ar);
+    x_res          += r.smul(eta);
+    r              -= ar.smul(eta);
+  }
+
+  return x_res;
+}
+
+inline Tensor conjuaget_gradient(
+    Tensor const &a,
+    Tensor const &b,
+    Tensor const &x0,
+    size_t const max_iter = 1000,
+    float const tol       = std::numeric_limits<float>::epsilon()
+)
+{
+  Tensor x_res{x0};
+
+  auto r = b - a * x_res;
+  auto p = r;
+
+  for (size_t i{0}; i < max_iter; i++)
+  {
+    auto const r_t = r.transpose();
+    auto const r_e = r_t * r;
+    if (std::sqrt(r_e.cpu().front()) < tol)
+    {
+      return x_res;
+    }
+
+    auto const p_t    = p.transpose();
+    auto const ap     = a * p;
+    auto const alpha  = r_e.cdiv(p_t * ap);
+    x_res            += p.smul(alpha);
+    r                -= ap.smul(alpha);
+    auto const beta   = (r.transpose() * r).cdiv(r_e);
+    p                 = r + p.smul(beta);
   }
 
   return x_res;

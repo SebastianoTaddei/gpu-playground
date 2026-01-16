1593339432366198dbd27e68eec9c92d07c3ea22
Sebastiano Taddei <61469576+SebastianoTaddei@users.noreply.github.com>
2026-01-16 15:02:08 +0100

    feat: added cg and gd benchmarks

---
+++ benchmarks/algorithms/benchmark_gd.cpp
@@ -0,0 +1,40 @@
+#include <string>
+
+#include "catch2/benchmark/catch_benchmark.hpp"
+#include "catch2/catch_test_macros.hpp"
+
+#include "algorithms.hpp"
+#include "device.hpp"
+#include "tensor.hpp"
+
+using namespace gpu_playground;
+
+TEST_CASE("algorithms: gradient descent", "[algorithms]")
+{
+  auto const devices = make_devices();
+
+  constexpr size_t rows{100};
+  constexpr size_t cols{100};
+  Shape const a_shape{rows, cols};
+  Shape const b_shape{cols, 1};
+  Tensor a  = Tensor::Rand(a_shape, devices[DeviceIdx::SERIAL]);
+  Tensor b  = Tensor::Rand(b_shape, devices[DeviceIdx::SERIAL]);
+  Tensor x0 = Tensor(b_shape, devices[DeviceIdx::SERIAL]);
+
+  a = a.transpose() * a;
+
+  for (auto const &device : devices)
+  {
+    if (device != nullptr)
+    {
+      a.to(device);
+      b.to(device);
+      x0.to(device);
+
+      BENCHMARK(std::string(get_device_name(device->type())))
+      {
+        return gradient_descent(a, b, x0);
+      };
+    }
+  }
+}
